b0899b831be637b2a74833ac5d0440a76f919849
samson tedla <samitedla23@gmail.com>
2022-03-05 23:20:28 +0000

    created model and patient class

---
+++ models/patient.py
@@ -0,0 +1,11 @@
+from models.base_model import BaseModel
+
+class Patient(Basemodel):
+    first_name = ""
+    last_name = ""
+    gender = ""
+    age = ""
+    phone_number = ""
+    residence_city = ""
+    medical_record = ""
+    prescriptions = ""
